f822b63b12f02a74082572f1d61eacf62e7e9a30
Cedric Marie <djced96@gmail.com>
2022-05-07 17:16:07 +0400

    Created Edit Form and routes

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,7 +1,46 @@
 class TaskController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
-    
+    @tasks = Task.all
   end
 
+  def new
+  end
 
+  # Create a task
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_index_path
+  end
+
+  # Show a task (READ)
+  def show
+  end
+
+  def edit
+  end
+
+  # update task
+  def update
+    @task.update(task_params)
+    redirect_to task_index_path
+  end
+
+  # delete task
+  def destroy
+    @task.destroy
+    redirect_to task_index_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'task#index'
+  # link to create task
+  get 'tasks/new', to: 'task#new', as: :new_task
+  # Show List of Task
+  get 'tasks/:id', to: 'task#show', as: :task
+
+  # link to edit task
+  get 'tasks/:id/edit', to: 'task#edit', as: :edit_task
+  patch 'tasks/:id/edit', to: 'task#update'
+
 end
